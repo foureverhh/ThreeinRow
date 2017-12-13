e11d8dff7edb7a988aaacf8e0059197f694bfbc5
Yuanheng Li <foureverhh@Yuanhengs-MacBook-Air.local>
2017-12-13 11:09:14 +0100

    It seems except for a machine player it works.

--- src/Nackademin/Board.java
+++ src/Nackademin/Board.java
@@ -1,4 +1,5 @@
 package Nackademin;
+import com.sun.codemodel.internal.JOp;
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
 import javax.swing.*;
@@ -9,17 +10,17 @@ import java.lang.*;
 public class Board {
 
 
-    protected int rowAndCol, blockAmount;   //To decide how big a board would be, to count how many blocks left on the board
-
-    protected String[][] board;       //To make a board
+    private int rowAndCol, blockAmount;   //To decide how big a board would be, to count how many blocks left on the board
+    protected int winPoint;
+    private String[][] board;       //To make a board
     protected int x, y;               //To create the coordinate for a chess
 
     protected String chessLabel;
-
+    private String winner;
     protected boolean gameIsOn;     //To judge whether a new board is needed
     protected boolean gameRestart;      //To judge whether a game should go on or not
 
-    protected int winPoint;
+
 
 // Constructor
     public Board(int rowAndCol)
@@ -34,14 +35,6 @@ public class Board {
     }
 
 //Methods
-    //Create an empty two dimentional array
-    public void brandnewBoard()
-    {
-
-        for(int i = 0; i<this.rowAndCol; i++)
-            for(int j =0; j<this.rowAndCol; j++)
-                board[i][j]=(" ");
-    }
 
     //print out an empty board
     public void printBoard(Player p1,Player p2)
@@ -49,7 +42,7 @@ public class Board {
 
         if(gameIsOn)
         {
-        //show all chess
+            //show all chess
             System.out.println("Welcome "+p1.getName()+" and "+p2.getName()+". Enjoy your game!!");
             showBoard();
 
@@ -62,6 +55,16 @@ public class Board {
             showBoard();
         }
     }
+
+    //Create an empty two dimentional array
+    public void brandnewBoard()
+    {
+
+        for(int i = 0; i<this.rowAndCol; i++)
+            for(int j =0; j<this.rowAndCol; j++)
+                board[i][j]=(" ");
+    }
+
 
     public void showBoard()
     {
@@ -86,21 +89,53 @@ public class Board {
         }
     }
 
-    //To check how many blocks left,when no block left turn gameIsOn to false
-    public void leftBlockAmount()
-    {
 
-        blockAmount--;
-        if(blockAmount==0)
+
+    //To check whether a game should begin again
+    public void gameRestart(Player playerCheck,Player otherPlayer)
+    {
+        String s=JOptionPane.showInputDialog("Do you players want to restart the game?(Y/N)");
+        if(s.toLowerCase().equals("n"))
         {
-            gameRestart = true;    //No blocks left need to restart
-            gameIsOn = false;      //No blocks left, game stops.
-            System.out.println("All blocks are used up");
+            gameIsOn = false;      //No winner and no blocks left, game stops.
+            gameRestart = true;      //No winner and no blocks left, game needed to restart.
+            JOptionPane.showMessageDialog(null,"Game is over!"+
+                    "\n"+playerCheck.getName()+" won "+playerCheck.winPoint+" times."+
+                    "\n"+otherPlayer.getName()+" won "+otherPlayer.winPoint+" times.");
+        }
+    }
+
+
+
+    //If here is a winner,show winner's name on the board
+    //or show game is even, and print
+    public void showGameResult(Player playerCheck,Player otherPlayer)
+    {
+        if(winner!=null)
+        {
+            gameIsOn = false;        //Got a winner, game stops.
+            playerCheck.winPoint++;
+
+            JOptionPane.showMessageDialog(null, "Congratulation, winner is: " + winner
+                    + "\n" + winner + " won " + playerCheck.winPoint + " times." + "/n" +
+                    otherPlayer.getName() + " won " + otherPlayer.winPoint + " times.");
+
+            gameRestart(playerCheck,otherPlayer);
+
+        }else if(blockAmount==0)
+        {
+            gameIsOn = false;
+            JOptionPane.showMessageDialog(null, "All blocks are used up, No winner this round!"
+                    + "\n" + playerCheck.getName() + " won " + playerCheck.winPoint + " times." + "/n" +
+                    otherPlayer.getName() + " won " + otherPlayer.winPoint + " times.");
+
+            gameRestart(playerCheck,otherPlayer);
+
         }
     }
 
     //Create a chess coordinate and place  the  chess  on board
-    public void placeChess(Player p) //(Player thisPlayer, Player otherPlayer)
+    public void placeChess(Player p)
     {
         p.setChessCoordinateX();
         p.setChessCoordinateY();
@@ -113,41 +148,32 @@ public class Board {
         }
 
         this.board[p.x][p.y] = p.chessLabel;
-        leftBlockAmount();
-        System.out.println(blockAmount);
+        blockAmount--;
+        //System.out.println(blockAmount);
     }
+
+
 
     //Scan the board to check who is the winner
-    public void checkWinner(Player p)
+    public void checkWinner(Player playerCheck,Player otherPlayer)
     {
-        //horizontallyCheck(p);             // Check board in the direction of -
-        //verticallyCheck(p);               // Check board in the direction of |
-        //diagonallyCheckBackslash(p);      // Check board in the direction of \
-        diagonallyCheckSlash(p);          // Check board in the direction of /
+        horizontallyCheck(playerCheck,otherPlayer);             // Check board in the direction of -
+        verticallyCheck(playerCheck,otherPlayer);               // Check board in the direction of |
+        diagonallyCheckBackslash(playerCheck,otherPlayer);      // Check board in the direction of \
+        diagonallyCheckSlash(playerCheck,otherPlayer);          // Check board in the direction of /
     }
 
-    //Show winner's name on the board
-    public void showWinner(Player p)
-    {
-        this.gameIsOn = false;      //Got a winner, game stops.
-        this.gameRestart = true;      //Got a winner, game need to restart.
-        p.winPoint++;
-        JOptionPane.showMessageDialog(null,"Congratulation, winner is: "+ p.getName()
-                +"\nYou have won "+p.winPoint+" times.");
-    }
 
 
     //To check whether player wins horizontally
-    public void horizontallyCheck(Player p)
+    public void horizontallyCheck(Player playerCheck,Player otherPlayer)
     {
         int i=0;
         while(i<board.length)
         {
             int k=0;
             for(int j=0;j<board.length;j++) {
-                //if each previous block holds the player's chess label
-                //and all the next block has same  value to its previous one horizontally.
-                if (board[i][j].equals(p.chessLabel)) {
+                if (board[i][j].equals(playerCheck.chessLabel)) {
                     k++;
                 } else {
                     break;
@@ -156,7 +182,8 @@ public class Board {
             }
             if(k==board.length)
             {
-                showWinner(p);
+                winner = playerCheck.getName(); //To judge whether here is a winner or not
+                showGameResult(playerCheck,otherPlayer);
                 break;
             }
             i++;
@@ -165,16 +192,16 @@ public class Board {
     }
 
     //To check whether player wins vertically
-    public void verticallyCheck(Player p)
+    public void verticallyCheck(Player playerCheck,Player otherPlayer)
     {
         int j=0;
         while(j<board.length)
         {
             int k = 0;
             for(int i=0;i<board.length;i++){
-                if(board[i][j].equals(p.chessLabel))
+                if(board[i][j].equals(playerCheck.chessLabel))
                 {
-                        k++;
+                    k++;
                 }else
                 {
                     break;
@@ -182,7 +209,8 @@ public class Board {
             }
             if(k==board.length)
             {
-                showWinner(p);
+                winner = playerCheck.getName();     //To judge whether here is a winner or not
+                showGameResult(playerCheck,otherPlayer);
                 break;
             }
             j++;
@@ -190,17 +218,18 @@ public class Board {
     }
 
     //To check whether player wins diagonally, slash
-    public void diagonallyCheckBackslash(Player p)
+    public void diagonallyCheckBackslash(Player playerCheck,Player otherPlayer)
     {
         int k=0;
         for(int i=0;i<board.length;i++)
         {
-            if(board[i][i].equals(p.chessLabel))
+            if(board[i][i].equals(playerCheck.chessLabel))
             {
                 k++;
                 if(k==board.length)
                 {
-                    showWinner(p);
+                    winner = playerCheck.getName(); //To judge whether here is a winner or not
+                    showGameResult(playerCheck,otherPlayer);
                     break;
                 }
             }else
@@ -211,17 +240,18 @@ public class Board {
     }
 
     //To check whether player wins diagonally, backslash
-    public void diagonallyCheckSlash(Player p)
+    public void diagonallyCheckSlash(Player playerCheck,Player otherPlayer)
     {
         int k=0;
         for(int i=0;i<board.length;i++)
         {
-            if(board[i][board.length-1-i].equals(p.chessLabel))
+            if(board[i][board.length-1-i].equals(playerCheck.chessLabel))
             {
                 k++;
                 if(k==board.length)
                 {
-                    showWinner(p);
+                    winner = playerCheck.getName(); //To judge whether here is a winner or not
+                    showGameResult(playerCheck,otherPlayer);
                     break;
                 }
             }else
@@ -230,32 +260,5 @@ public class Board {
             }
         }
 
-        /*for(int i=0;i<board.length;i++)
-        {
-            int k=0; //Wrong, if k is here would k never rise
-
-            if(board[i][board.length-1-i].equals(p.chessLabel))
-            {
-                k++;
-            }else
-            {
-                break;
-            }
-
-            if(k==board.length)
-            {
-                showWinner(p);
-                break;
-            }
-        }*/
-    }
-
-
-    public int getBlockAmount() {
-        return blockAmount;
-    }
-
-    public int getRowAndCol() {
-        return rowAndCol;
     }
 }
--- src/Nackademin/Main.java
+++ src/Nackademin/Main.java
@@ -7,7 +7,7 @@ public class Main
 
     public static void main(String[] args) {
         //Create board and players
-        Player p1 = new Player(4);
+        Player p1 = new Player(3);
         Player p2 = new Player();
 
         p1.setName();
@@ -15,29 +15,32 @@ public class Main
         p2.setName();
         p2.setChessLabel();
 
-        p1.printBoard(p1,p2);  //Print out a new empty chessboard
-
-        p1.gameIsOn = true; // Game is moving further on
-
-        while(p1.gameIsOn)
+        while(!p1.gameRestart)
         {
+            p1.printBoard(p1, p2);  //Print out a new empty chessboard
 
-            //Player 1 choose his/her own block and place the chess
-            p1.placeChess(p1);
-            //Print out the current board
-            p1.printBoard(p1,p2);
-            //Check winner
-            p1.checkWinner(p1);
-            if(!p1.gameIsOn)
-                break;
+            p1.gameIsOn = true; // Game is moving further on
 
-            //Player 2 choose his//her own block and place the chess
-            p1.placeChess(p2);
-            //Print out the current board
-            p1.printBoard(p1,p2);
-            p1.checkWinner(p2);
-            if(!p1.gameIsOn)
-                break;
+            while (p1.gameIsOn) {
+
+                //Player 1 choose his/her own block and place the chess
+                p1.placeChess(p1);
+                //Print out the current board
+                p1.printBoard(p1, p2);
+                //Check winner
+                p1.checkWinner(p1, p2);
+                if (!p1.gameIsOn)
+                    break;
+
+                //Player 2 choose his//her own block and place the chess
+                p1.placeChess(p2);
+                //Print out the current board
+                p1.printBoard(p1, p2);
+                p1.checkWinner(p2, p1);
+                if (!p1.gameIsOn)
+                    break;
+
+            }
 
         }
     }
